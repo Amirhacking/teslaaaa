1a49774168b311d19b10b90ddfc269a9a2254d28
Amirhacking <41117749+Amirhacking@users.noreply.github.com>
2021-05-15 08:24:51 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,8 @@
+do local data = {
+  BotUsername = "@TESLA_ANTISPAM_BOT",
+  sudo_users = {358165791,1806205761},
+  token = "1846248575:AAESnYnWZC2cnhMGL9ammmUqL5BoeLoXTm4",
+  channel_id ="@iranize_link"
+}
+return data
+end
---
+++ libs/time_today.php
@@ -0,0 +1,5 @@
+<?php
+include('./php/jdf.php');
+echo jdate('H:i:s Y F j ,l', time()) . PHP_EOL ;
+
+
